a5c71c0400943e1a09556fe4623e0d7c12c213ae
pravin-sarule <pravin.sarule@nexintelai.com>
2025-11-04 12:33:34 +0530

    .

--- services/document-service/services/gcsService.js
+++ services/document-service/services/gcsService.js
@@ -1,42 +1,4 @@
 
-
-// const { bucket, fileInputBucket } = require('../src/config/gcs'); // Make sure this is correctly importing your initialized bucket
-
-// /**
-//  * Upload a file buffer to Google Cloud Storage
-//  */
-// exports.uploadToGCS = async (filename, buffer, folder = 'uploads', isBatch = false) => {
-//   const targetBucket = isBatch ? fileInputBucket : bucket;
-//   const timestamp = Date.now();
-//   const destination = `${folder}/${timestamp}_${filename}`;
-//   const file = targetBucket.file(destination);
-
-//   await file.save(buffer, {
-//     resumable: false,
-//     contentType: 'auto',
-//     metadata: {
-//       cacheControl: 'public, max-age=31536000'
-//     },
-//   });
-
-//   // Return GCS path (not signed URL)
-//   return { url: file.publicUrl(), path: destination };
-// };
-
-// /**
-//  * Generate a temporary signed URL for download
-//  */
-// exports.getSignedUrl = async (gcsPath, expiresInSeconds = 300) => {
-//   const file = bucket.file(gcsPath);
-
-//   const [url] = await file.getSignedUrl({
-//     version: 'v4',
-//     action: 'read',
-//     expires: Date.now() + expiresInSeconds * 1000, // Default 5 minutes
-//   });
-
-//   return url;
-// };
 const { bucket, fileInputBucket } = require('../config/gcs');
 const path = require('path');
 
